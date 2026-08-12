86fa94046dc2f2a840dcf27d64e9cc25e4bd6894
wyattb79 <none>
2026-08-12 16:39:06 -0400

    Add

--- src/read_dynamo.py
+++ src/read_dynamo.py
@@ -5,7 +5,10 @@ import os
 from boto3.dynamodb.conditions import Key
 
 region = os.environ.get('REGION')
-ec2_client = boto3.client('ec2', region_name=region)
+dynamodb = boto3.resource("dynamodb", region_name=region)
+sqs_client = boto3.client('sqs')
+TABLE_NAME = 'jumpbox_access'
+table = dynamodb.Table(TABLE_NAME)
 
 logger = logging.getLogger()
 log_level = os.environ.get("LAMBDA_LOG_LEVEL", "INFO").upper()
@@ -15,23 +18,42 @@ queue_url = os.environ['SQS_QUEUE_URL']
 def handler(event, context):
 
   for record in event['Records']:
-    message_body = json.loads(record['body'])
-    instance_id = message_body['detail']['instance-id']
-    state = message_body['detail']['state']
-    logger.info(f"Instance: {instance_id} now {state}")
-
     try:
-      logger.info("Terminate")
-
+      message_body = json.loads(record.get("body", {}))
+      detail = message_body.get("detail", {})
+      instance_id = detail.get("instance-id")
+      state = detail.get("state")
+  
+      if not instance_id:
+        logger.warning("No instance-id in record.  Skipping")
+        continue
+  
+      logger.debug(f"Instance: {instance_id} now {state}")
+  
       response = table.query(
         KeyConditionExpression=Key('InstanceId').eq(f"{instance_id}")
       )
-      Item = response.get('Items', [])
-      logger.info(f"{Item}")
-      remote_sg = Item[remote_sg]
-      jumpbox_sg = Item[jumpbox_sg]
-      logger.info(f"{response}")
 
+      items = response.get('Items', [])
+
+      if not items:
+        logger.warning("No items found in Dynamo")
+        continue
+
+      item = items[0]
+      remote_sg = item.get("remote_sg")
+      jumpbox_sg = item.get("jumpbox_sg")
+
+      message_data = {
+        "instance_id": instance_id,
+        "remote_sg": remote_sg,
+        "jumpbox_sg": sg
+      }
+
+      response = sqs_client.send_message(
+        QueueUrl=queue_url,
+        MessageBody = json.dumps(message_data)
+      )
 
     except Exception as e:
       return {
